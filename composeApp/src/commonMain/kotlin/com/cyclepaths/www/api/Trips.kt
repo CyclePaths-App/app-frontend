593b8ddf772859e51c9e80e5b9bc40380e1ab66c
CreatorOfCryptids <dannyanddave5000@gmail.com>
2025-11-12 18:07:41 -0500

    Did things...

--- composeApp/src/commonMain/kotlin/com/cyclepaths/www/api/Trips.kt
+++ composeApp/src/commonMain/kotlin/com/cyclepaths/www/api/Trips.kt
@@ -6,7 +6,6 @@ import io.ktor.client.HttpClient
 import io.ktor.client.call.body
 import io.ktor.client.request.delete
 import io.ktor.client.request.get
-import io.ktor.client.request.parameter
 import io.ktor.client.request.post
 import io.ktor.client.request.put
 import io.ktor.client.request.setBody
