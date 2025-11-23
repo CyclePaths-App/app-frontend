aad5492b9d2917946ad4bae66a1e1a8261346a6e
CreatorOfCryptids <dannyanddave5000@gmail.com>
2025-11-23 01:24:19 -0500

    Cleaned up code for readability.

--- composeApp/src/commonMain/kotlin/com/cyclepaths/www/RecordingTrip.kt
+++ composeApp/src/commonMain/kotlin/com/cyclepaths/www/RecordingTrip.kt
@@ -57,7 +57,7 @@ fun RecordingTripDemo() {
 }
 
 @Composable
-fun RecordingButton(onClick: () -> Unit, message: String) {
+fun RecordingButton(onClick: () -> Unit, message: String, enabled: Boolean) {
     Button(
         onClick = onClick,
         border = BorderStroke(1.dp, Color.Black),
@@ -66,6 +66,7 @@ fun RecordingButton(onClick: () -> Unit, message: String) {
         colors = ButtonDefaults.buttonColors(
             containerColor = Color(0xFFFF3958C8)
         ),
+        enabled = enabled
     ) {
         Text(
             message,
@@ -84,6 +85,49 @@ fun RecordingButton(onClick: () -> Unit, message: String) {
 
 @Composable
 fun RecordingTrip(navController: NavController, tripType: TripType) {
+    val josefinSansFamily = FontFamily(
+        Font(Res.font.josefin_sans_regular, FontWeight.Normal),
+        Font(Res.font.josefin_sans_bold, FontWeight.Bold),
+        Font(Res.font.josefin_sans_italic, FontWeight.Normal, FontStyle.Italic)
+    )
+
+    val api = remember { BackendAPI(BACKEND_URL) }
+
+    var errorMessage by remember { mutableStateOf<String?>(null) }
+    var trip by remember { mutableStateOf(ArrayList<BackendAPI.Location>()) }
+
+    val scope = rememberCoroutineScope()
+
+    val tracker = remember { MobileGeolocator() }
+    val trackingStatus by tracker.trackingStatus.collectAsState(initial = null)
+
+    // Launch effect runs in the background
+    LaunchedEffect(Unit) {
+        tracker.trackingStatus.collect { status ->
+            when (status) { // When a status changes, the collection sends that here
+                is TrackingStatus.Error -> {
+                    val error = status.cause
+                    errorMessage = "TRACKING ERROR: $error"
+                }
+
+                TrackingStatus.Idle -> {}   // Don't need to do anything if it is idle.
+                TrackingStatus.Tracking -> {}   // Waiting for an update, no need to do anything.
+                is TrackingStatus.Update -> {
+                    // Unwrap and rewrap location information.
+                    val latitude = status.location.coordinates.latitude
+                    val longitude = status.location.coordinates.longitude
+                    val time = Clock.System.now().toLocalDateTime(TimeZone.UTC)
+                        .toString()// Whomst designed this API?
+
+                    val currentLocation = BackendAPI.Location(latitude, longitude, time)
+
+                    // Add to list.
+                    trip.add(currentLocation)
+                }
+            }
+        }
+    }
+
     MaterialTheme {
         Box(
             modifier = Modifier.background(Color(0xFF2E377C))
@@ -96,45 +140,7 @@ fun RecordingTrip(navController: NavController, tripType: TripType) {
                 verticalArrangement = Arrangement.SpaceEvenly,
                 horizontalAlignment = Alignment.CenterHorizontally,
             ) {
-                val josefinSansFamily = FontFamily(
-                    Font(Res.font.josefin_sans_regular, FontWeight.Normal),
-                    Font(Res.font.josefin_sans_bold, FontWeight.Bold),
-                    Font(Res.font.josefin_sans_italic, FontWeight.Normal, FontStyle.Italic)
-                )
-                val api = remember { BackendAPI(BACKEND_URL) }
-                var errorMessage by remember { mutableStateOf<String?>(null) }
-                var locationList by remember { mutableStateOf(ArrayList<BackendAPI.Location>()) }
 
-                val scope = rememberCoroutineScope()
-
-                val geolocator = remember { MobileGeolocator() }
-                val trackingStatus by geolocator.trackingStatus
-                    .collectAsState(initial = null)
-
-                // Launch effect runs in the background
-                LaunchedEffect(Unit) {
-                    geolocator.trackingStatus.collect { status ->
-                        when (status) { // When a status changes, the collection sends that here
-                            is TrackingStatus.Error -> {
-                                val error = status.cause
-                                errorMessage = "TRACKING ERROR: $error"
-                            }
-
-                            TrackingStatus.Idle -> {}
-                            TrackingStatus.Tracking -> {}
-                            is TrackingStatus.Update -> {
-                                val latitude = status.location.coordinates.latitude
-                                val longitude = status.location.coordinates.longitude
-                                val time = Clock.System.now().toLocalDateTime(TimeZone.UTC)
-                                    .toString()// Whomst designed this API?
-
-                                val currentLocation = BackendAPI.Location(latitude, longitude, time)
-
-                                locationList.add(currentLocation)
-                            }
-                        }
-                    }
-                }
 
                 Box {
                     val textStyle = TextStyle(
@@ -165,90 +171,40 @@ fun RecordingTrip(navController: NavController, tripType: TripType) {
                         )
                     )
                 }
-                
+
                 if (trackingStatus == TrackingStatus.Idle) {
-                    RecordingButton(onClick = {
-                        scope.launch(Dispatchers.IO) {
-                            geolocator.startTracking(
-                                LocationRequest(Priority.HighAccuracy, 1000)
-                            )
-                        }
-                    }, "Start Tracking")
+                    RecordingButton(
+                        onClick = {
+                            scope.launch(Dispatchers.IO) {
+                                tracker.startTracking(
+                                    LocationRequest(Priority.HighAccuracy, 1000)
+                                )
+                            }
+                        }, "Start Tracking",
+                        true
+                    )
                 } else {
-                    Button(
+                    RecordingButton(
                         onClick = {
                             scope.launch {
-                                geolocator.stopTracking()
-//                            val standardList =
-//                                listOf(
-//                                    BackendAPI.Location(
-//                                        latitude = 42.686261,
-//                                        longitude = -73.828025,
-//                                        time = LocalDateTime(2025, 9, 20, 18, 5, 0)
-//                                            .toInstant(TimeZone.UTC)
-//                                            .toString()
-//                                    ),
-//                                    BackendAPI.Location(
-//                                        latitude = 42.686945,
-//                                        longitude = -73.827349,
-//                                        time = LocalDateTime(2025, 9, 20, 18, 5, 15)
-//                                            .toInstant(TimeZone.UTC)
-//                                            .toString()
-//                                    ),
-//                                    BackendAPI.Location(
-//                                        latitude = 42.687378,
-//                                        longitude = -73.826919,
-//                                        time = LocalDateTime(2025, 9, 20, 18, 5, 30)
-//                                            .toInstant(TimeZone.UTC)
-//                                            .toString()
-//                                    )
-//                                )
-                                api.createTrip(userId = 1, locationList, tripType).onSuccess {
-                                    try {
-                                        navController.navigate(
-                                            when (tripType) {
-                                                TripType.bike -> "cyclestats"
-                                                TripType.walk -> "walkstats"
-                                            }
-                                        )
-                                    } catch (e: Error) {
-                                        errorMessage = e.toString()
-                                    }
+                                tracker.stopTracking()// Stop tracking to prevent overlapping thread nonsense.
 
+                                api.createTrip(userId = 1, trip, tripType).onSuccess {
+                                    navController.navigate(
+                                        when (tripType) {
+                                            TripType.bike -> "cyclestats"
+                                            TripType.walk -> "walkstats"
+                                        }
+                                    )
                                 }.onFailure {
-                                    errorMessage =
-                                        it.message + " LocationList has " + locationList.size + " entries."
-
+                                    errorMessage = it.message
                                 }
                             }
                         },
-                        border = BorderStroke(1.dp, Color.Black),
-                        shape = RoundedCornerShape(100),
-                        modifier = Modifier.padding(15.dp).size(300.dp),
-                        colors = ButtonDefaults.buttonColors(
-                            containerColor = Color(0xFFFF3958C8)
-                        ),
-                    ) {
-                        Text(
-                            "End Trip",
-                            color = Color.White,
-                            style = TextStyle(
-                                fontFamily = FontFamily(
-                                    Font(Res.font.josefin_sans_regular, FontWeight.Normal),
-                                    Font(Res.font.josefin_sans_bold, FontWeight.Bold),
-                                    Font(
-                                        Res.font.josefin_sans_italic,
-                                        FontWeight.Normal,
-                                        FontStyle.Italic
-                                    )
-                                ),
-                                fontSize = 35.sp,
-                            )
-                        )
-                    }
+                        "End trip",
+                        trip.isNotEmpty() // Needs entries in the trip to avoid Bad Request.
+                    )
                 }
-
-
                 errorMessage?.let {
                     Text(
                         it,
